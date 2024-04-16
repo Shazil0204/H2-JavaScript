a9582ef9c2937b707f83f6472ab410c2ac0ffcdb
Shazil0204 <ssq.zbc.23@gmail.com>
2024-04-16 09:09:55 +0200

    Both sides are completed

---
+++ Music/Flute.js
@@ -0,0 +1,9 @@
+class FLute extends Instrument {
+
+    constructor(name, holes, material) {
+        super(name);
+        this.holes = holes;
+        this.material = material;
+    }
+
+}
---
+++ Music/Saxophone.js
@@ -0,0 +1,8 @@
+class Saxophone extends Instrument{
+
+    constructor(name, material){
+        super(name);
+        this.material = material;
+    }
+
+}
